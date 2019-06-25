5a193091c366791f314aae3f8ea8b16b0806b5e8
Arturo Gil <charturo.gil@gmail.com>
2019-06-24 23:55:48 -0500

    Finished until adding template

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,56 @@
 const socket = io();
 
-socket.on("countUpdated", count => {
-  console.log("The count has been updated!", count);
+//Elements
+const $messageForm = document.querySelector("#chat");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML
+
+socket.on("message", message => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message
+  });
+  $messages.insertAdjacentHTML("beforeend", html)
 });
 
-document.querySelector("#increment").addEventListener("click", () => {
-  console.log("Clicked");
-  socket.emit("increment");
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if(error){
+      return console.log(error);
+    }
+
+    console.log("Message delivered");
+  });
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if(!navigator.geolocation){
+    return alert("Geolocation is not supported by your browser");
+  }
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {    
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      $sendLocationButton.removeAttribute("disabled");
+      console.log('Location shared!')
+    });
+  });
 });
